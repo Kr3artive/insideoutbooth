b99ca3b55fa44d63bacc48f59c533ebb851f965e
Abiye <abiyerowland@gmail.com>
2024-11-29 13:08:43 +0200

    new stuffs

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,17 +1,16 @@
+import Another from "../Images/Another.jpg";
+import Pic1 from "../Images/PIC1.jpg";
+import Pic3 from "../Images/PIC3.jpg";
+import Pic4 from "../Images/PIC4.jpg";
+import Pic5 from "../Images/PIC5.jpg";
+
 const Gallery = () => {
-  
-  const images = [
-    "/Images/Another.jpg",
-    "/Images/PIC1.jpg",
-    "/Images/PIC3.jpg",
-    "/Images/PIC4.jpg",
-    "/Images/PIC5.jpg",
-  ];
+  const images = [Another, Pic1, Pic3, Pic4, Pic5];
 
   return (
-    <div className="bg-gray-100 p-6 overflow-x-hidden">
+    <div className="bg-gray-100 p-6">
       <h1 className="text-3xl font-bold text-center mb-6">Gallery</h1>
-      <div className="flex space-x-4">
+      <div className="flex space-x-4 overflow-x-scroll scrollbar-hide">
         {images.map((src, index) => (
           <div
             key={index}
